3f0f09737fe60346e49d1a5987de39c41761c7dd
farisgozi <fariscraft709@gmail.com>
2026-03-27 21:44:38 +0700

    implement base auth

--- auth/auth.ts
+++ auth/auth.ts
@@ -1,7 +1,8 @@
 import { Header, Gateway } from 'encore.dev/api';
 import { authHandler } from 'encore.dev/auth';
 import { APIError } from 'encore.dev/api';
-
+import { api } from 'encore.dev/api';
+import { getAuthData } from '~encore/auth';
 
 interface AuthParams {
     authorization: Header<"Authorization">;
@@ -10,6 +11,37 @@ interface AuthParams {
 interface AuthData {
     userID: string;
 }
+
+interface PingResponse {
+    ok: true;
+}
+
+interface isAuthResponse {
+    isAuthenticated: boolean;
+}
+
+
+export const ping = api({
+    expose: true, method: "GET", path: "/auth/ping"
+}, 
+    async (): Promise<PingResponse> => {
+        return { ok: true };
+    }
+
+);
+
+export const isAuth = api({
+    expose: true, method: "GET", path: "/auth/is_auth"
+}, 
+    async (): Promise<isAuthResponse> => {
+        const auth = getAuthData();
+        if (!auth) {
+            return { isAuthenticated: false };
+        }
+        return { isAuthenticated: true };
+    }
+);
+
 
 export const auth = authHandler<AuthParams, AuthData>(
     async (params) => {
